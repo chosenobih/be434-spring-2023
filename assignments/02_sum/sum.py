ad792bf643b936960f46d6c5c736da3af2126f89
Chosen Obih <obihchosen@yahoo.com>
2023-02-08 15:53:46 -0700

    created sum.py
    Still contains error

---
+++ assignments/02_sum/sum.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Author : Chosen Obih
+Email : chosenobih@arizona.edu
+Date   : 2023-02-05
+Purpose: A Python program that will sum one or more required integer values
+"""
+
+import argparse
+
+
+# --------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='A program that sums one or more integer values',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('numbs',
+                        metavar='int',
+                        nargs='+',
+                        help='Numbers to add')
+
+    return parser.parse_args()
+
+
+# --------------------------------------------------
+def main():
+    """main function to run the program"""
+
+    args = get_args()
+    numbs = args.numbs
+    numbers = []
+    total = 0
+    for numb in numbs:
+        total += numb
+        numbers.append(str(numb))
+    sum = ' + '.join(numbers)
+    print('sum')
+# --------------------------------------------------
+if __name__ == '__main__':
+    main()
