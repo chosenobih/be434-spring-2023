97856f233bd70ce042d630464b4fbc815acec9b4
Bonnie Hurwitz <bhurwitz@arizona.edu>
2023-02-16 11:47:37 -0800

    Updated to help fix errors

--- assignments/03_solfege/solfege.py
+++ assignments/03_solfege/solfege.py
@@ -17,7 +17,8 @@ def get_args():
         description='solfege',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('Solfege',
+    # make sure your arguments are always lowercase Solfege -> solfege
+    parser.add_argument('solfege',
                         nargs='+',
                         metavar='str',
                         type=str,
@@ -31,17 +32,36 @@ def main():
     """Main function to run the program"""
 
     args = get_args()
-    texts = args.texts
-    syllable = {'Do': 'A deer, a female deer',
-              'Re': 'A drop of golden sun',
-              'Mi': 'A name I call myself',
-              'Fa': 'A long long way to run',
-              'Sol': 'A needle pulling thread',
-              'La': 'A note to follow sol',
-              'Ti': 'A drink with jam and bread'}
-    for text in args.texts:
-        print(syllable.get(text, text), end='')
-    print()
+
+    # be sure to use the argument you created above.
+    # texts = args.texts   ->  texts = args.solfege
+    texts = args.solfege
+
+    # needed to correct the spacing, Python requires 4 spaces after the 
+    # dictionary is defined
+    syllable = {
+        'Do': 'A deer, a female deer',
+        'Re': 'A drop of golden sun',
+        'Mi': 'A name I call myself',
+        'Fa': 'A long long way to run',
+        'Sol': 'A needle pulling thread',
+        'La': 'A note to follow sol',
+        'Ti': 'A drink with jam and bread',
+    }
+
+    # I changed this to "texts" that you assigned it to above
+    # you can also use "for text in args.solfege:" It just needed to match
+    # what you assigned in the get_args above
+    for text in texts:
+        # You need to check that the text is in syllable first
+        #print(syllable.get(text, text), end='')
+        if text in syllable:
+            print(f'{text}, {syllable.get(text)}')
+        else:
+            # tell the user you don't have the note to print
+            print("Check out the README.md to see what to print here")
+
+
 # --------------------------------------------------
 if __name__ == '__main__':
     main()
